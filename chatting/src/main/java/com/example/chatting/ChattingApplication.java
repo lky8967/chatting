224707eed52f1f1befabf397815c67015719e642
lky8967 <lky8967@gmail.com>
2022-09-14 20:08:39 +0900

    한국시간 맞추기

--- chatting/src/main/java/com/example/chatting/ChattingApplication.java
+++ chatting/src/main/java/com/example/chatting/ChattingApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 
 @EnableCaching
 @SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class ChattingApplication {
 
+    //한국시간
+    @PostConstruct
+    public void timeZone() {
+        // timezone Asia/Seoul 셋팅
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(ChattingApplication.class, args);
     }
