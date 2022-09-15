d5c5dfd7ad2c97b473719c5585947a31ae8fdbd7
lky8967 <lky8967@gmail.com>
2022-09-15 13:58:30 +0900

    timestamp추가

--- chatting/src/main/java/com/example/chatting/ChattingApplication.java
+++ chatting/src/main/java/com/example/chatting/ChattingApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 
 @EnableCaching
+@EnableJpaAuditing // DateTimeProvider 인터페이스를 상속하여 커스터마이징한다고 나와있습니다
 @SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 public class ChattingApplication {
 
