23935c3ebfb1323e2a8aedf8ea16eb8deb6ae764
lky8967 <lky8967@gmail.com>
2022-09-16 11:42:58 +0900

    no message

---
+++ chatting/src/main/java/com/example/chatting/config/XSSFilter.java
@@ -0,0 +1,23 @@
+//package com.example.chatting.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.log4j.Log4j2;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//
+//@RequiredArgsConstructor
+//@Configuration
+////@EnableWebMvc
+//@Log4j2
+//public class XSSFilter {
+//    private final ObjectMapper objectMapper;
+//
+//    @Bean
+//    public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
+//        ObjectMapper copy = objectMapper.copy();
+//        copy.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+//        return new MappingJackson2HttpMessageConverter(copy);
+//    }
+//}
