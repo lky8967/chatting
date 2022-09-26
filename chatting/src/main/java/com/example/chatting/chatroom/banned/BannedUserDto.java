f003c08216e6c42036b931ed088a7f1e175849a3
lky8967 <lky8967@gmail.com>
2022-09-26 16:41:03 +0900

    차단유저 id 값 주기

--- chatting/src/main/java/com/example/chatting/chatroom/banned/BannedUserDto.java
+++ chatting/src/main/java/com/example/chatting/chatroom/banned/BannedUserDto.java
@@ -8,14 +8,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class BannedUserDto {
 
-    private Long bannedId;
+    private Long bannedUserId;
     private String nickname;
     private String profile;
 
     public static BannedUserDto createFrom(BannedUser banndedUser) {
 
         BannedUserDto dto = new BannedUserDto();
-        dto.bannedId = banndedUser.getId();
+        dto.bannedUserId = banndedUser.getBannedUser().getId();
         dto.nickname = banndedUser.getBannedUser().getNickname();
         dto.profile = banndedUser.getBannedUser().getUserImgUrl();
 
