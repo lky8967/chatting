568a2f32c16197dfbdc36bc1d770a1f04425715a
lky8967 <lky8967@gmail.com>
2022-09-28 15:11:15 +0900

    xss 필터 추가

--- chatting/src/main/java/com/example/chatting/config/HTMLCharacterEscapes.java
+++ chatting/src/main/java/com/example/chatting/config/HTMLCharacterEscapes.java
@@ -1,49 +1,49 @@
-//package com.example.chatting.config;
-//
-//import com.fasterxml.jackson.core.SerializableString;
-//import com.fasterxml.jackson.core.io.CharacterEscapes;
-//import com.fasterxml.jackson.core.io.SerializedString;
-//import lombok.extern.log4j.Log4j2;
-//import org.apache.commons.text.StringEscapeUtils;
-//
-//
-//@Log4j2
-//public class HTMLCharacterEscapes extends CharacterEscapes {
-//
-//    private final int[] asciiEscapes;
-//    public HTMLCharacterEscapes() {
-//        // 1. XSS 방지 처리할 특수 문자 지정
-//        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
-//        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
-//        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
-//    }
-//
-//    @Override
-//    public int[] getEscapeCodesForAscii() {
-//        return asciiEscapes;
-//    }
-//
-//    @Override
-//    public SerializableString getEscapeSequence(int ch) {
-//
-//        SerializedString serializedString = null;
-//        char charAt = (char) ch;
-//        //emoji jackson parse 오류에 따른 예외 처리
-//        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
-//            StringBuilder sb = new StringBuilder();
-//            sb.append("\\u");
-//            sb.append(String.format("%04x",ch));
-//            serializedString = new SerializedString(sb.toString());
-//        } else {
-//            serializedString = new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
-//        }
-//        return serializedString;
-//    }
-//}
-//
-//
+package com.example.chatting.config;
+
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
+import com.fasterxml.jackson.core.io.SerializedString;
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.text.StringEscapeUtils;
+
+
+@Log4j2
+public class HTMLCharacterEscapes extends CharacterEscapes {
+
+    private final int[] asciiEscapes;
+    public HTMLCharacterEscapes() {
+        // 1. XSS 방지 처리할 특수 문자 지정
+        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+        return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int ch) {
+
+        SerializedString serializedString = null;
+        char charAt = (char) ch;
+        //emoji jackson parse 오류에 따른 예외 처리
+        if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("\\u");
+            sb.append(String.format("%04x",ch));
+            serializedString = new SerializedString(sb.toString());
+        } else {
+            serializedString = new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+        }
+        return serializedString;
+    }
+}
+
+
